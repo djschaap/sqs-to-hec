f9ee022a2110adaeba7647f40e942d55e4fb86cb
Doug Schaapveld <djschaap@users.noreply.github.com>
2020-05-29 09:35:02 -0500

    Refactor source layout

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,93 +1,49 @@
 package main
 
 import (
-	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"github.com/djschaap/sqs-to-hec/pkg/receivesqs"
-	"github.com/djschaap/sqs-to-hec/pkg/sendhec"
-	hec "github.com/fuyufjh/splunk-hec-go"
-	"net/http"
+	"github.com/djschaap/sqs-to-hec"
 	"os"
 	"regexp"
 )
 
 var (
-	build_dt string
-	commit   string
-	version  string
+	buildDt string
+	commit  string
+	version string
 )
 
 func main() {
-	fmt.Println("sqs-to-hec  Version:", version, " Commit:", commit,
-		" Built at:", build_dt)
-	var max_receive_messages int64 = 2 // TODO create MAX_RECEIVE_MESSAGES env var
-	var max_receive_wait int64 = 2     // in seconds
-	//max_send_events := 2 // TODO create MAX_SEND_EVENTS env var
+	printVersion()
 
-	src_queue_url := os.Getenv("SRC_QUEUE")
-	has_src_queue, _ := regexp.MatchString(`^https`, src_queue_url)
-	if !has_src_queue {
+	srcQueue := os.Getenv("SRC_QUEUE")
+	hasSrcQueue, _ := regexp.MatchString(`^https`, srcQueue)
+	if !hasSrcQueue {
 		fmt.Println("ERROR: SRC_QUEUE must be set")
 		os.Exit(1)
 	}
 
-	hec_url := os.Getenv("HEC_URL")
-	hec_token := os.Getenv("HEC_TOKEN")
-	has_hec_url, _ := regexp.MatchString(`\S`, hec_url)
-	has_hec_token, _ := regexp.MatchString(`\S`, hec_token)
+	hecUrl := os.Getenv("HEC_URL")
+	hecToken := os.Getenv("HEC_TOKEN")
 
-	receivesqs.OpenSvc()
-
-	if has_hec_url && has_hec_token {
-		hec_client := hec.NewCluster(
-			[]string{hec_url},
-			hec_token,
-		)
-		if true { // TODO set InsecureSkipVerify from env var
-			hec_client.SetHTTPClient(&http.Client{Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			}})
-		}
+	hecConfig := sqstohec.HecConfig{
+		Token: hecToken,
+		Url:   hecUrl,
+	}
+	sqsConfig := sqstohec.SqsConfig{
+		Url: srcQueue,
 	}
 
-	for {
-		sqs_result, err := receivesqs.ReceiveMessages(src_queue_url, max_receive_messages, max_receive_wait)
+	app := sqstohec.New(
+		hecConfig,
+		sqsConfig,
+	)
 
-		if err != nil {
-			fmt.Println("ERROR from ReceiveMessages:", err)
-			os.Exit(2)
-			// TODO retry/attempt recovery?
-		}
+	app.RunOnce()
+}
 
-		var hec_messages []hec.Event
-		if len(sqs_result.Messages) > 0 {
-			hec_messages = sendhec.FormatForHEC(sqs_result.Messages)
-			//debug_bytes, _ := json.Marshal(sqs_result.Messages)
-			//fmt.Println("  DEBUG:", string(debug_bytes))
-		}
-
-		if len(hec_messages) > 0 {
-			var send_err error
-			if has_hec_url && has_hec_token {
-				send_err = sendhec.SendToHEC(hec_url, hec_token, hec_messages)
-				if send_err != nil {
-					fmt.Println("ERROR from SendToHEC:", send_err)
-					os.Exit(3)
-					// TODO retry/attempt recovery?
-				}
-			} else {
-				fmt.Println("WARNING: HEC_URL and/or HEC_TOKEN are missing")
-				if hec_messages != nil {
-					for _, m := range hec_messages {
-						debug_bytes, _ := json.Marshal(m)
-						fmt.Println("NOT-SENT-TO-HEC:", string(debug_bytes))
-					}
-				}
-			}
-			if send_err == nil {
-				receivesqs.DeleteMessages(src_queue_url, sqs_result)
-			}
-		}
-	}
+func printVersion() {
+	fmt.Println("sqs-to-hec cli  Version:",
+		version, " Commit:", commit,
+		" Built at:", buildDt)
 }
---
+++ sqstohec.go
@@ -0,0 +1,100 @@
+package sqstohec
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/djschaap/sqs-to-hec/receivesqs"
+	"github.com/djschaap/sqs-to-hec/sendhec"
+	hec "github.com/fuyufjh/splunk-hec-go"
+	"net/http"
+	"os"
+	"regexp"
+)
+
+type HecConfig struct {
+	Token string
+	Url   string
+}
+
+type SqsConfig struct {
+	Url string
+}
+
+type sess struct {
+	HecConfig HecConfig
+	SqsConfig SqsConfig
+}
+
+func (self *sess) RunOnce() {
+	var maxReceiveMessages int64 = 2 // TODO create MAX_RECEIVE_MESSAGES env var
+	var maxReceiveWait int64 = 2     // in seconds
+	//maxSendEvents := 2 // TODO create MAX_SEND_EVENTS env var
+
+	receivesqs.OpenSvc()
+
+	hasHecUrl, _ := regexp.MatchString(`\S`, self.HecConfig.Url)
+	hasHecToken, _ := regexp.MatchString(`\S`, self.HecConfig.Token)
+	if hasHecUrl && hasHecToken {
+		hecClient := hec.NewCluster(
+			[]string{self.HecConfig.Url},
+			self.HecConfig.Token,
+		)
+		if true { // TODO set InsecureSkipVerify from env var
+			hecClient.SetHTTPClient(&http.Client{Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}})
+		}
+	}
+
+	for {
+		mqResult, err := receivesqs.ReceiveMessages(self.SqsConfig.Url, maxReceiveMessages, maxReceiveWait)
+
+		if err != nil {
+			fmt.Println("ERROR from ReceiveMessages:", err)
+			os.Exit(2)
+			// TODO retry/attempt recovery?
+		}
+
+		var hecMmessages []hec.Event
+		if len(mqResult.Messages) > 0 {
+			hecMmessages = sendhec.FormatForHEC(mqResult.Messages)
+			//debugBytes, _ := json.Marshal(mqResult.Messages)
+			//fmt.Println("  DEBUG:", string(debugBytes))
+		}
+
+		if len(hecMmessages) > 0 {
+			var sendErr error
+			if hasHecUrl && hasHecToken {
+				sendErr = sendhec.SendToHEC(self.HecConfig.Url, self.HecConfig.Token, hecMmessages)
+				if sendErr != nil {
+					fmt.Println("ERROR from SendToHEC:", sendErr)
+					os.Exit(3)
+					// TODO retry/attempt recovery?
+				}
+			} else {
+				fmt.Println("WARNING: HEC_URL and/or HEC_TOKEN are missing")
+				if hecMmessages != nil {
+					for _, m := range hecMmessages {
+						debugBytes, _ := json.Marshal(m)
+						fmt.Println("NOT-SENT-TO-HEC:", string(debugBytes))
+					}
+				}
+			}
+			if sendErr == nil {
+				receivesqs.DeleteMessages(self.SqsConfig.Url, mqResult)
+			}
+		}
+	}
+}
+
+func New(
+	hecConfig HecConfig,
+	sqsConfig SqsConfig,
+) sess {
+	s := sess{
+		HecConfig: hecConfig,
+		SqsConfig: sqsConfig,
+	}
+	return s
+}
