fd515973439bab4e932af29b364797e4bdee13c7
Doug Schaapveld <djschaap@users.noreply.github.com>
2020-05-29 09:33:46 -0500

    Separate SQS/MQ and HEC headers
    - allow processing of metric events

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.13
 
 require (
 	github.com/aws/aws-sdk-go v1.28.11
-	github.com/fuyufjh/splunk-hec-go v0.3.3
-	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/fuyufjh/splunk-hec-go v0.3.4-0.20190414090710-10df423a9f36
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 )
--- pkg/sendhec/sendhec.go
+++ pkg/sendhec/sendhec.go
@@ -2,74 +2,89 @@ package sendhec
 
 import (
 	"crypto/tls"
-	//"encoding/json"
+	"encoding/json"
 	//"fmt"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	hec "github.com/fuyufjh/splunk-hec-go"
+	//"github.com/kr/pretty"
+	//"log"
 	"net/http"
 	"strconv"
 	"time"
 )
 
+var trace_mq bool
+
 func FormatForHEC(sqs_messages []*sqs.Message) []hec.Event {
 	var hec_messages []hec.Event
 
 	for _, m := range sqs_messages {
-		//message_json_bytes, _ := json.Marshal(m)
-		//fmt.Println("Message:\n", string(message_json_bytes))
-		//fmt.Println("  Body:", string(*m.Body))
+		var mq_message_body map[string]interface{}
+		mq_message_body_bytes := []byte(*m.Body)
+		json.Unmarshal(mq_message_body_bytes, &mq_message_body)
+		//pretty.Log("TRACE mq_message_body = ", mq_message_body) // DEBUG
 
-		hec_message := hec.NewEvent(string(*m.Body))
+		var hec_message *hec.Event
+		if _, ok := mq_message_body["event"]; ok {
+			//log.Println("DEBUG 30: NEW style") // expect HEC headers within MQ body; allows metrics
+			hec_message = hec.NewEvent(mq_message_body["event"])
+			if mq_message_body["host"] != nil {
+				v := mq_message_body["host"]
+				hec_message.SetHost(v.(string))
+			}
+			if mq_message_body["index"] != nil {
+				v := mq_message_body["index"]
+				hec_message.SetIndex(v.(string))
+			}
+			if mq_message_body["source"] != nil {
+				v := mq_message_body["source"]
+				hec_message.SetSource(v.(string))
+			}
+			if mq_message_body["sourcetype"] != nil {
+				v := mq_message_body["sourcetype"]
+				hec_message.SetSourceType(v.(string))
+			}
+			if mq_message_body["fields"] != nil {
+				v := mq_message_body["fields"]
+				hec_message.SetFields(v.(map[string]interface{}))
+			}
+		} else {
+			//log.Println("DEBUG 40: OLD style") // use MQ attributes for HEC headers
+			hec_message = hec.NewEvent(string(*m.Body))
+			if m.MessageAttributes["host"] != nil {
+				v := string(*m.MessageAttributes["host"].StringValue)
+				hec_message.SetHost(v)
+			}
+			if m.MessageAttributes["index"] != nil {
+				v := string(*m.MessageAttributes["index"].StringValue)
+				hec_message.SetIndex(v)
+			}
+			if m.MessageAttributes["source"] != nil {
+				v := string(*m.MessageAttributes["source"].StringValue)
+				hec_message.SetSource(v)
+			}
+			if m.MessageAttributes["sourcetype"] != nil {
+				v := string(*m.MessageAttributes["sourcetype"].StringValue)
+				hec_message.SetSourceType(v)
+			}
+		}
+		//time.Sleep(3 * 100000) // DEBUG
 
-		//if m.MessageAttributes["customer_code"] != nil {
-		//fmt.Println("  customer_code:", string(*m.MessageAttributes["customer_code"].StringValue))
-		//}
-		if m.MessageAttributes["host"] != nil {
-			v := string(*m.MessageAttributes["host"].StringValue)
-			//fmt.Println("  host:", v)
-			hec_message.SetHost(v)
-		}
-		if m.MessageAttributes["index"] != nil {
-			v := string(*m.MessageAttributes["index"].StringValue)
-			//fmt.Println("  index:", v)
-			hec_message.SetIndex(v)
-		}
-		if m.MessageAttributes["source"] != nil {
-			v := string(*m.MessageAttributes["source"].StringValue)
-			//fmt.Println("  source:", v)
-			hec_message.SetSource(v)
-		}
-		if m.MessageAttributes["sourcetype"] != nil {
-			v := string(*m.MessageAttributes["sourcetype"].StringValue)
-			//fmt.Println("  sourcetype:", v)
-			hec_message.SetSourceType(v)
-		}
 		if m.Attributes["SentTimestamp"] != nil {
 			i, err := strconv.Atoi(*m.Attributes["SentTimestamp"])
 			if err == nil {
 				time_s := int64(i / 1000)
-				time_us := ( int64(i) - time_s * 1000 ) * 1000000
+				time_us := (int64(i) - time_s*1000) * 1000000
 				v := time.Unix(time_s, time_us)
 				//fmt.Println("  SentTimeStamp:", v)
 				hec_message.SetTime(v)
 			}
 		}
 
-		//if len(body["time"]) > 0 {
-		//	hec_message.SetTime(body["time"])
-		// cannot use body["time"] (type string) as type time.Time in argument to hec_message.SetTime
-		//}
-
 		// SetFields ?
 		// https://docs.splunk.com/Documentation/Splunk/8.0.1/Data/HECExamples
 
 		hec_messages = append(hec_messages, *hec_message)
-
-		//for k, m := range hec_messages {
-		//fmt.Println("  host[", k, "]: ", m.Host)
-		//debug_bytes, _ := json.Marshal(m)
-		//fmt.Println("  Message ", i, ":\n", string(debug_bytes))
-		//}
 	}
 
 	return hec_messages
